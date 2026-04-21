bdfbd8ef943345a57d701e90cc510f38833fdf45
Agnissan Isaac Valen <valenbouge@gmail.com>
2026-04-21 17:44:17 +0000

    ok

--- js/app.js
+++ js/app.js
@@ -1,25 +1,4 @@
 
-// debut pop up
-const myPopup = document.getElementById('customPopup');
-const closeBtn = document.getElementById('closeBtn');
-
-// Affiche le popup après 3 secondes
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        myPopup.style.display = 'flex';
-    }, 3000);
-});
-
-// Fermeture
-closeBtn.addEventListener('click', () => {
-    myPopup.style.display = 'none';
-});
-
-
-
-
-
-// fin pop up
 
 
 function goto(page){
